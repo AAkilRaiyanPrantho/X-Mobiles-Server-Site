1ec03f25f377d82082d41e023a389d277e81f89d
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2023-10-21 17:04:44 +0600

    Data Entry to data base sucessful

--- index.js
+++ index.js
@@ -29,9 +29,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const carCollection = client.db('carDB').collection('car');
+
     app.post('/product', async(req,res)=>{
       const newProduct = req.body;
       console.log(newProduct);
+      const result = await carCollection.insertOne(newProduct);
+      res.send(result);
     })
 
 
